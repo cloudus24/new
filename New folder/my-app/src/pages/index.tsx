22aa79e3922ef70eae385343303036fd313a6d84
jaydip.sonani@cloudusinfotech.com <harshsuhagiya.cloudus22@gmail.com>
2024-10-23 10:13:45 +0530

    first commit

---
+++ New folder/my-app/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import New from '../components/new';
+
+
+const Home = () => {
+  return (
+    <div className="container position-relative p-30">
+      <New />
+    </div>
+  );
+}
+
+export default Home;
