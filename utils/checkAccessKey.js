3f39933fd5f19b93fa8e7dcc6f40e1cd1dbd39b7
jaydip.sonani@cloudusinfotech.com <harshsuhagiya.cloudus22@gmail.com>
2024-11-29 12:12:32 +0530

    first commit

--- utils/checkAccessKey.js
+++ utils/checkAccessKey.js
@@ -1,18 +1,19 @@
 exports.checkAccess = async (req, res, next) => {
     try {
-        const key = req.body.key || req.query.key || req.headers.key
+        const key = req.body.key || req.query.key || req.headers.key; // Get key from body, query, or headers
         console.log('key :>> ', key);
+        
         if (key) {
-            if (key === process.env.KEY) {
-                next()
+            if (key === process.env.KEY) { // Compare the key with the one in environment variables
+                return next(); // Call next middleware/controller
             } else {
-                return res.status(401).json({ message: "unauthorized access !!" });
+                return res.status(401).json({ message: "Unauthorized access!" });
             }
         } else {
-            return res.status(401).json({ message: "unauthorized access !!" });
+            return res.status(401).json({ message: "Unauthorized access!" });
         }
-
     } catch (error) {
-        return res.status(500).json({ message: "unauthorized access !!" });
+        console.error('Error in checkAccess:', error);
+        return res.status(500).json({ message: "Internal Server Error" });
     }
-}
+};
